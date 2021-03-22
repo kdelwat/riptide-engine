3464487f0b720f8e4484950416c5416ac305da3a
Cadel Watson <cadel.watson@geora.io>
2021-03-23 06:23:01 +1100

    Update make move tests

--- src/make_move_test.zig
+++ src/make_move_test.zig
@@ -20,11 +20,6 @@ test "Quiet move" {
 
     const artifacts = make_move.makeMove(&starting_pos, m);
 
-//    var list = std.ArrayList(u8).init(test_allocator);
-//    defer list.deinit();
-//    debug.toFEN(starting_pos, &list) catch unreachable;
-//    std.debug.print("FEN: {s}\n", .{list.items});
-
     std.testing.expectEqualSlices(u64, starting_pos.board.boards[0..], expected_pos.board.boards[0..]);
     expect(starting_pos.eq(expected_pos));
 
@@ -42,187 +37,191 @@ test "Capture" {
     expect(m.is(MoveType.capture));
 
     const artifacts = make_move.makeMove(&starting_pos, m);
-    starting_pos.board.debug();
-    expected_pos.board.debug();
     expect(starting_pos.eq(expected_pos));
 
     make_move.unmakeMove(&starting_pos, m, artifacts);
 
     expect(starting_pos.eq(starting_pos_saved));
 }
-//
-//test "Double pawn push" {
-//    var starting_pos = position.fromFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1") catch unreachable;
-//
-//    const starting_pos_saved = starting_pos;
-//
-//    const expected_pos = position.fromFEN("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1") catch unreachable;
-//    const m = move.createDoublePawnPush(20, 52);
-//
-//    const artifacts = make_move.makeMove(&starting_pos, m);
-//
-//    expect(starting_pos.eq(expected_pos));
-//
-//    make_move.unmakeMove(&starting_pos, move.createDoublePawnPush(20, 52), artifacts);
-//    expect(starting_pos.eq(starting_pos_saved));
-//}
-//
-//test "Promotion" {
-//    var starting_pos = position.fromFEN("rnbq1bnr/pppBP1p1/6kp/5p2/3Q4/8/PPP2PPP/RNB1K1NR w KQ - 0 9") catch unreachable;
-//
-//    const starting_pos_saved = starting_pos;
-//
-//    const expected_pos = position.fromFEN("rnbqQbnr/pppB2p1/6kp/5p2/3Q4/8/PPP2PPP/RNB1K1NR b KQ - 0 9") catch unreachable;
-//    const m = move.createPromotionMove(100, 116, piece.PieceType.queen);
-//
-//    const artifacts = make_move.makeMove(&starting_pos, m);
-//
-//    expect(starting_pos.eq(expected_pos));
-//
-//    make_move.unmakeMove(&starting_pos, move.createPromotionMove(100, 116, piece.PieceType.queen), artifacts);
-//    expect(starting_pos.eq(starting_pos_saved));
-//}
-//
-//test "En passant capture" {
-//    var starting_pos = position.fromFEN("rnbqkbnr/pp1p2pp/5p2/2pPp3/4P3/8/PPP2PPP/RNBQKBNR w KQkq c6 0 4") catch unreachable;
-//
-//    const starting_pos_saved = starting_pos;
-//
-//    const expected_pos = position.fromFEN("rnbqkbnr/pp1p2pp/2P2p2/4p3/4P3/8/PPP2PPP/RNBQKBNR b KQkq - 0 4") catch unreachable;
-//    const m = move.createEnPassantCaptureMove(67, 82);
-//
-//    const artifacts = make_move.makeMove(&starting_pos, m);
-//
-//    expect(starting_pos.eq(expected_pos));
-//
-//    make_move.unmakeMove(&starting_pos, move.createEnPassantCaptureMove(67, 82), artifacts);
-//    expect(starting_pos.eq(starting_pos_saved));
-//}
-//
-//test "Promotion capture" {
-//    var starting_pos = position.fromFEN("rnbqkbnr/pP4pp/5p2/3pp3/4P3/8/PPP2PPP/RNBQKBNR w KQkq - 0 6") catch unreachable;
-//
-//    const starting_pos_saved = starting_pos;
-//
-//    const expected_pos = position.fromFEN("rnNqkbnr/p5pp/5p2/3pp3/4P3/8/PPP2PPP/RNBQKBNR b KQkq - 0 6") catch unreachable;
-//    const m = move.createPromotionCaptureMove(97, 114, piece.PieceType.knight);
-//
-//    const artifacts = make_move.makeMove(&starting_pos, m);
-//
-//    expect(starting_pos.eq(expected_pos));
-//
-//    make_move.unmakeMove(&starting_pos, move.createPromotionCaptureMove(97, 114, piece.PieceType.knight), artifacts);
-//    expect(starting_pos.eq(starting_pos_saved));
-//}
-//
-//test "Losing castle rights from king" {
-//    var starting_pos = position.fromFEN("rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2") catch unreachable;
-//
-//    const starting_pos_saved = starting_pos;
-//
-//    const expected_pos = position.fromFEN("rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPPKPPP/RNBQ1BNR b kq - 1 2") catch unreachable;
-//    const m = move.createQuietMove(4, 20);
-//
-//    const artifacts = make_move.makeMove(&starting_pos, m);
-//
-//    expect(starting_pos.eq(expected_pos));
-//
-//    make_move.unmakeMove(&starting_pos, move.createQuietMove(4, 20), artifacts);
-//    expect(starting_pos.eq(starting_pos_saved));
-//}
-//
-//test "Losing castle rights from rook" {
-//    var starting_pos = position.fromFEN("rnbqkbnr/ppppppp1/7p/8/8/6PP/PPPPPP2/RNBQKBNR b KQkq - 0 2") catch unreachable;
-//
-//    const starting_pos_saved = starting_pos;
-//
-//    const expected_pos = position.fromFEN("rnbqkbn1/pppppppr/7p/8/8/6PP/PPPPPP2/RNBQKBNR w KQq - 1 3") catch unreachable;
-//    const m = move.createQuietMove(119, 103);
-//
-//    const artifacts = make_move.makeMove(&starting_pos, m);
-//
-//    expect(starting_pos.eq(expected_pos));
-//
-//    make_move.unmakeMove(&starting_pos, move.createQuietMove(119, 103), artifacts);
-//    expect(starting_pos.eq(starting_pos_saved));
-//}
-//
-//test "Castle queenside" {
-//    var starting_pos = position.fromFEN("rnbqkb1r/ppp1pppp/8/3p2B1/3Pn3/2N5/PPPQPPPP/R3KBNR w KQkq - 2 5") catch unreachable;
-//
-//    const starting_pos_saved = starting_pos;
-//
-//    const expected_pos = position.fromFEN("rnbqkb1r/ppp1pppp/8/3p2B1/3Pn3/2N5/PPPQPPPP/2KR1BNR b kq - 3 5") catch unreachable;
-//    const m = move.QUEEN_CASTLE;
-//
-//    const artifacts = make_move.makeMove(&starting_pos, m);
-//
-//    expect(starting_pos.eq(expected_pos));
-//
-//    make_move.unmakeMove(&starting_pos, move.QUEEN_CASTLE, artifacts);
-//    expect(starting_pos.eq(starting_pos_saved));
-//}
-//
-//test "Castle kingside" {
-//    var starting_pos = position.fromFEN("rnbqk2r/ppp2ppp/3bpB2/3p4/3PN3/8/PPPQPPPP/2KR1BNR b kq - 2 7") catch unreachable;
-//
-//    const starting_pos_saved = starting_pos;
-//
-//    const expected_pos = position.fromFEN("rnbq1rk1/ppp2ppp/3bpB2/3p4/3PN3/8/PPPQPPPP/2KR1BNR w - - 3 8") catch unreachable;
-//    const m = move.KING_CASTLE;
-//
-//    const artifacts = make_move.makeMove(&starting_pos, m);
-//
-//    expect(starting_pos.eq(expected_pos));
-//
-//    make_move.unmakeMove(&starting_pos, move.KING_CASTLE, artifacts);
-//    expect(starting_pos.eq(starting_pos_saved));
-//}
-//
-//test "Buggy case" {
-//    var starting_pos = position.fromFEN("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q2/PPPBBPpP/2R1K2R b Kkq - 1 2") catch unreachable;
-//
-//    const starting_pos_saved = starting_pos;
-//
-//    const expected_pos = position.fromFEN("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q2/PPPBBP1P/2R1K1qR w Kkq - 0 3") catch unreachable;
-//    const m = move.createPromotionMove(22, 6, piece.PieceType.queen);
-//
-//    const artifacts = make_move.makeMove(&starting_pos, m);
-//
-//    expect(starting_pos.eq(expected_pos));
-//
-//    make_move.unmakeMove(&starting_pos, move.createPromotionMove(22, 6, piece.PieceType.queen), artifacts);
-//    expect(starting_pos.eq(starting_pos_saved));
-//}
-//
-//test "Another bug" {
-//    var starting_pos = position.fromFEN("8/8/8/8/k7/8/2Kp4/2R5 b - - 1 3") catch unreachable;
-//
-//    const starting_pos_saved = starting_pos;
-//
-//    const expected_pos = position.fromFEN("8/8/8/8/8/1k6/2Kp4/2R5 w - - 2 4") catch unreachable;
-//    const m = move.createQuietMove(48, 33);
-//
-//    const artifacts = make_move.makeMove(&starting_pos, m);
-//
-//    expect(starting_pos.eq(expected_pos));
-//
-//    make_move.unmakeMove(&starting_pos, move.createQuietMove(48, 33), artifacts);
-//    expect(starting_pos.eq(starting_pos_saved));
-//}
-//
-//test "Underpromotion capture" {
-//    var starting_pos = position.fromFEN("8/8/8/8/k7/8/2Kp4/2R5 b - - 1 3") catch unreachable;
-//
-//    const starting_pos_saved = starting_pos;
-//
-//    const expected_pos = position.fromFEN("8/8/8/8/k7/8/2K5/2b5 w - - 0 4") catch unreachable;
-//    const m = move.createPromotionCaptureMove(19, 2, piece.PieceType.bishop);
-//
-//    const artifacts = make_move.makeMove(&starting_pos, m);
-//
-//    expect(starting_pos.eq(expected_pos));
-//
-//    make_move.unmakeMove(&starting_pos, move.createPromotionCaptureMove(19, 2, piece.PieceType.bishop), artifacts);
-//    expect(starting_pos.eq(starting_pos_saved));
-//}
+
+test "Double pawn push" {
+    var starting_pos = position.fromFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1") catch unreachable;
+
+    const starting_pos_saved = starting_pos;
+
+    const expected_pos = position.fromFEN("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1") catch unreachable;
+    const m = Move.initDoublePawnPush(12, 28, Color.white);
+
+    const artifacts = make_move.makeMove(&starting_pos, m);
+
+    expect(starting_pos.eq(expected_pos));
+
+    make_move.unmakeMove(&starting_pos, m, artifacts);
+    expect(starting_pos.eq(starting_pos_saved));
+}
+
+test "Promotion" {
+    var starting_pos = position.fromFEN("rnbq1bnr/pppBP1p1/6kp/5p2/3Q4/8/PPP2PPP/RNB1K1NR w KQ - 0 9") catch unreachable;
+
+    const starting_pos_saved = starting_pos;
+
+    const expected_pos = position.fromFEN("rnbqQbnr/pppB2p1/6kp/5p2/3Q4/8/PPP2PPP/RNB1K1NR b KQ - 0 9") catch unreachable;
+    const m = Move.initPromotion(52, 60, Color.white, PieceType.queen);
+
+    const artifacts = make_move.makeMove(&starting_pos, m);
+
+    expect(starting_pos.eq(expected_pos));
+
+    make_move.unmakeMove(&starting_pos, m, artifacts);
+    expect(starting_pos.eq(starting_pos_saved));
+}
+
+test "En passant capture" {
+    var starting_pos = position.fromFEN("rnbqkbnr/pp1p2pp/5p2/2pPp3/4P3/8/PPP2PPP/RNBQKBNR w KQkq c6 0 4") catch unreachable;
+
+    const starting_pos_saved = starting_pos;
+
+    const expected_pos = position.fromFEN("rnbqkbnr/pp1p2pp/2P2p2/4p3/4P3/8/PPP2PPP/RNBQKBNR b KQkq - 0 4") catch unreachable;
+    const m = Move.initEnPassant(35, 42, Color.white);
+
+    const artifacts = make_move.makeMove(&starting_pos, m);
+
+    expect(starting_pos.eq(expected_pos));
+
+    make_move.unmakeMove(&starting_pos, m, artifacts);
+    expect(starting_pos.eq(starting_pos_saved));
+}
+
+
+test "Promotion capture" {
+    var starting_pos = position.fromFEN("rnbqkbnr/pP4pp/5p2/3pp3/4P3/8/PPP2PPP/RNBQKBNR w KQkq - 0 6") catch unreachable;
+
+    const starting_pos_saved = starting_pos;
+
+    const expected_pos = position.fromFEN("rnNqkbnr/p5pp/5p2/3pp3/4P3/8/PPP2PPP/RNBQKBNR b KQkq - 0 6") catch unreachable;
+    const m = Move.initPromotionCapture(49, 58, Color.white, PieceType.knight, PieceType.bishop);
+
+    const artifacts = make_move.makeMove(&starting_pos, m);
+
+    expect(starting_pos.eq(expected_pos));
+
+    make_move.unmakeMove(&starting_pos, m, artifacts);
+    expect(starting_pos.eq(starting_pos_saved));
+}
+
+
+test "Losing castle rights from king" {
+    var starting_pos = position.fromFEN("rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2") catch unreachable;
+
+    const starting_pos_saved = starting_pos;
+
+    const expected_pos = position.fromFEN("rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPPKPPP/RNBQ1BNR b kq - 1 2") catch unreachable;
+    const m = Move.initQuiet(4, 12, Color.white, PieceType.king);
+
+    const artifacts = make_move.makeMove(&starting_pos, m);
+
+    expect(starting_pos.eq(expected_pos));
+
+    make_move.unmakeMove(&starting_pos, m, artifacts);
+    expect(starting_pos.eq(starting_pos_saved));
+}
+
+
+test "Losing castle rights from rook" {
+    var starting_pos = position.fromFEN("rnbqkbnr/ppppppp1/7p/8/8/6PP/PPPPPP2/RNBQKBNR b KQkq - 0 2") catch unreachable;
+
+    const starting_pos_saved = starting_pos;
+
+    const expected_pos = position.fromFEN("rnbqkbn1/pppppppr/7p/8/8/6PP/PPPPPP2/RNBQKBNR w KQq - 1 3") catch unreachable;
+    const m = Move.initQuiet(63, 55, Color.black, PieceType.rook);
+
+    const artifacts = make_move.makeMove(&starting_pos, m);
+
+    expect(starting_pos.eq(expected_pos));
+
+    make_move.unmakeMove(&starting_pos, m, artifacts);
+    expect(starting_pos.eq(starting_pos_saved));
+}
+
+
+test "Castle queenside" {
+    var starting_pos = position.fromFEN("rnbqkb1r/ppp1pppp/8/3p2B1/3Pn3/2N5/PPPQPPPP/R3KBNR w KQkq - 2 5") catch unreachable;
+
+    const starting_pos_saved = starting_pos;
+
+    const expected_pos = position.fromFEN("rnbqkb1r/ppp1pppp/8/3p2B1/3Pn3/2N5/PPPQPPPP/2KR1BNR b kq - 3 5") catch unreachable;
+    const m = Move.initQueensideCastle(Color.white);
+
+    const artifacts = make_move.makeMove(&starting_pos, m);
+
+    expect(starting_pos.eq(expected_pos));
+
+    make_move.unmakeMove(&starting_pos, m, artifacts);
+    expect(starting_pos.eq(starting_pos_saved));
+}
+
+
+test "Castle kingside" {
+    var starting_pos = position.fromFEN("rnbqk2r/ppp2ppp/3bpB2/3p4/3PN3/8/PPPQPPPP/2KR1BNR b kq - 2 7") catch unreachable;
+
+    const starting_pos_saved = starting_pos;
+
+    const expected_pos = position.fromFEN("rnbq1rk1/ppp2ppp/3bpB2/3p4/3PN3/8/PPPQPPPP/2KR1BNR w - - 3 8") catch unreachable;
+    const m = Move.initKingsideCastle(Color.black);
+
+    const artifacts = make_move.makeMove(&starting_pos, m);
+
+    expect(starting_pos.eq(expected_pos));
+
+    make_move.unmakeMove(&starting_pos, m, artifacts);
+    expect(starting_pos.eq(starting_pos_saved));
+}
+
+test "Bug: promotion" {
+    var starting_pos = position.fromFEN("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q2/PPPBBPpP/2R1K2R b Kkq - 1 2") catch unreachable;
+
+    const starting_pos_saved = starting_pos;
+
+    const expected_pos = position.fromFEN("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q2/PPPBBP1P/2R1K1qR w Kkq - 0 3") catch unreachable;
+    const m = Move.initPromotion(14, 6, Color.black, PieceType.queen);
+
+    const artifacts = make_move.makeMove(&starting_pos, m);
+
+    expect(starting_pos.eq(expected_pos));
+
+    make_move.unmakeMove(&starting_pos, m, artifacts);
+    expect(starting_pos.eq(starting_pos_saved));
+}
+
+
+test "Bug: king movement" {
+    var starting_pos = position.fromFEN("8/8/8/8/k7/8/2Kp4/2R5 b - - 1 3") catch unreachable;
+
+    const starting_pos_saved = starting_pos;
+
+    const expected_pos = position.fromFEN("8/8/8/8/8/1k6/2Kp4/2R5 w - - 2 4") catch unreachable;
+    const m = Move.initQuiet(24, 17, Color.black, PieceType.king);
+
+    const artifacts = make_move.makeMove(&starting_pos, m);
+
+    expect(starting_pos.eq(expected_pos));
+
+    make_move.unmakeMove(&starting_pos, m, artifacts);
+    expect(starting_pos.eq(starting_pos_saved));
+}
+
+test "Underpromotion capture" {
+    var starting_pos = position.fromFEN("8/8/8/8/k7/8/2Kp4/2R5 b - - 1 3") catch unreachable;
+
+    const starting_pos_saved = starting_pos;
+
+    const expected_pos = position.fromFEN("8/8/8/8/k7/8/2K5/2b5 w - - 0 4") catch unreachable;
+    const m = Move.initPromotionCapture(11, 2, Color.black, PieceType.bishop, PieceType.rook);
+
+    const artifacts = make_move.makeMove(&starting_pos, m);
+
+    expect(starting_pos.eq(expected_pos));
+
+    make_move.unmakeMove(&starting_pos, m, artifacts);
+    expect(starting_pos.eq(starting_pos_saved));
+}
