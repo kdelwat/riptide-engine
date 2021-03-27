62deb127d658b90f5d2c9b001771b0a221d270f9
Cadel Watson <cadel.watson@geora.io>
2021-03-27 16:15:04 +1100

    Refactor evaluation tests and add eval NPS

--- src/evaluate_test.zig
+++ src/evaluate_test.zig
@@ -6,51 +6,27 @@ const expect = std.testing.expect;
 const expectEqual = std.testing.expectEqual;
 const test_allocator = std.testing.allocator;
 
-test "Pawn testing" {
-    const expected: i64 = 330;
-    var pos = position.fromFEN("8/8/8/8/4P3/3P4/2P5/8 w KQkq - 0 11", test_allocator) catch unreachable;
-    expect(evaluate(&pos) == expected);
-    pos.to_move = switch (pos.to_move) {
-        Color.white => Color.black,
-        Color.black => Color.white,
-    };
-    expect(evaluate(&pos) == -expected);
-}
+const EvaluateTestCase = struct {
+    position: []const u8,
+    expected: i64,
+};
 
-test "Knight, rook, bishop"  {
-    const expected: i64 = -685;
-    var pos = position.fromFEN("8/5n2/r2r4/8/8/6B1/3B4/8 w KQkq - 0 1", test_allocator) catch unreachable;
-    expect(evaluate(&pos) == expected);
-    pos.to_move = switch (pos.to_move) {
-        Color.white => Color.black,
-        Color.black => Color.white,
-    };
-    expect(evaluate(&pos) == -expected);
-}
+var EVALUATE_TEST_CASES = [4]EvaluateTestCase{
+    EvaluateTestCase{.position = "8/8/8/8/4P3/3P4/2P5/8 w KQkq - 0 11", .expected = 330},
+    EvaluateTestCase{.position = "8/5n2/r2r4/8/8/6B1/3B4/8 w KQkq - 0 1", .expected = -685},
+    EvaluateTestCase{.position = "8/8/8/8/8/8/8/K4k2 w - - 0 1", .expected = 60},
+    EvaluateTestCase{.position = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", .expected = 0},
+};
 
-test "Asymmetrical kings" {
-    const expected: i64 = 60;
-    var pos = position.fromFEN("8/8/8/8/8/8/8/K4k2 w - - 0 1", test_allocator) catch unreachable;
-
-    expectEqual(expected, evaluate(&pos));
-
-    pos.to_move = switch (pos.to_move) {
-        Color.white => Color.black,
-        Color.black => Color.white,
-    };
-    expectEqual(-expected, evaluate(&pos));
-}
-
-test "Starting position" {
-    const expected: i64 = 0;
-    var pos = position.fromFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", test_allocator) catch unreachable;
-
-    expectEqual(expected, evaluate(&pos));
-
-    pos.to_move = switch (pos.to_move) {
-        Color.white => Color.black,
-        Color.black => Color.white,
-    };
-
-    expectEqual(-expected, evaluate(&pos));
+test "evaluate" {
+    for (EVALUATE_TEST_CASES) |test_case| {
+        std.debug.print("evaluate: {s}\n", .{test_case.position});
+        var pos = position.fromFEN(test_case.position, test_allocator) catch unreachable;
+        expectEqual(test_case.expected, evaluate(&pos));
+        pos.to_move = switch (pos.to_move) {
+            Color.white => Color.black,
+            Color.black => Color.white,
+        };
+        expectEqual(-test_case.expected, evaluate(&pos));
+    }
 }
--- src/nps_test.zig
+++ src/nps_test.zig
@@ -3,6 +3,7 @@ const perft = @import("./perft.zig").perft;
 const std = @import("std");
 const expect = std.testing.expect;
 const test_allocator = std.testing.allocator;
+const evaluate = @import("evaluate.zig").evaluate;
 
 fn fromFEN(f: []const u8) position.Position {
     return position.fromFEN(f, test_allocator) catch unreachable;
@@ -13,4 +14,18 @@ test "move generation nodes per second" {
     expect(perft(&fromFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"), 5) == 4865609);
 
     std.debug.print("NPS: {}\n", .{@floatToInt(u64, @intToFloat(f64, 4865609) / (@intToFloat(f64, timer.read()) / @intToFloat(f64, std.time.ns_per_s)))});
+}
+
+test "evaluation nodes per second" {
+    const timer = std.time.Timer.start() catch unreachable;
+
+    var pos = position.fromFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", test_allocator) catch unreachable;
+
+    var i: u64 = 0;
+    while (i < 1000000) {
+        _ = evaluate(&pos);
+        i += 1;
+    }
+
+    std.debug.print("EPS: {}\n", .{@floatToInt(u64, @intToFloat(f64, 1000000) / (@intToFloat(f64, timer.read()) / @intToFloat(f64, std.time.ns_per_s)))});
 }
